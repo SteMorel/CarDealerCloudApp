8d73a43189e2d7c15a0ed58f439161710234fd48
SteMorel <stefano.morello@live.com>
2023-11-03 10:16:42 -0400

    Reviews not completed

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import logging
+from . import models
 # import related models here
 from requests.auth import HTTPBasicAuth
 from . import models
@@ -32,9 +33,78 @@ def get_request(url, **kwargs):
 # def get_dealers_from_cf(url, **kwargs):
 # - Call get_request() with specified arguments
 # - Parse JSON results into a CarDealer object list
+
 def get_dealers_from_cf(url, **kwargs):
     results = []
- 
+    state = kwargs.get("state")
+    if state:
+        json_result = get_request(url, state=state)
+    else:
+        json_result = get_request(url)
+    if json_result:
+        # Get the row list in JSON as dealers
+        dealers = json_result["result"]
+        # For each dealer object
+        for dealer in dealers:
+            # Get its content in `doc` object
+            dealer_doc = dealer["doc"]
+            #print(dealer_doc)
+            # Create a CarDealer object with values in `doc` object
+            dealer_obj = models.CarDealer(address=dealer_doc["address"], city=dealer_doc["city"], full_name=dealer_doc["full_name"],
+                                   id=dealer_doc["id"], lat=dealer_doc["lat"], long=dealer_doc["long"],
+                                   short_name=dealer_doc["short_name"],
+                                   st=dealer_doc["st"], zip=dealer_doc["zip"])
+            results.append(dealer_obj)
+
+    return results
+
+def get_dealer_reviews_from_cf(url, **kwargs):
+    results = []
+    id = kwargs.get("id")
+    print(url)
+    if id:
+        json_result = get_request(url, id=id)
+    else:
+        json_result = get_request(url)
+    print(json_result)
+    if json_result:
+        # Get all review data from the response
+        reviews = json_result["data"]["docs"]
+        # For every review in the response
+        for review in reviews:
+            # Create a DealerReview object from the data
+            # These values must be present
+            review_content = review["review"]
+            id = review["_id"]
+            name = review["name"]
+            purchase = review["purchase"]
+            dealership = review["dealership"]
+
+            try:
+                # These values may be missing
+                car_make = review["car_make"]
+                car_model = review["car_model"]
+                car_year = review["car_year"]
+                purchase_date = review["purchase_date"]
+
+                # Creating a review object
+                review_obj = models.DealerReview(dealership=dealership, id=id, name=name, 
+                                          purchase=purchase, review=review_content, car_make=car_make, 
+                                          car_model=car_model, car_year=car_year, purchase_date=purchase_date
+                                          )
+
+            except KeyError:
+                print("Something is missing from this review. Using default values.")
+                # Creating a review object with some default values
+                review_obj = models.DealerReview(
+                    dealership=dealership, id=id, name=name, purchase=purchase, review=review_content)
+
+            # Analysing the sentiment of the review object's review text and saving it to the object attribute "sentiment"
+            review_obj.sentiment = analyze_review_sentiments(review_obj.review)
+            print(f"sentiment: {review_obj.sentiment}")
+
+            # Saving the review object to the list of results
+            results.append(review_obj)
 
     return results
 
@@ -43,8 +113,9 @@ def get_dealers_from_cf(url, **kwargs):
 def get_dealer_reviews_by_id_from_cf(url, dealerId):
     results = []
     json_result = get_request(url, dealerId=dealerId)
+    print(json_result)
     if json_result:
-        reviews = json_result['docs']
+        reviews = json_result["data"]["docs"]
         for review in reviews:
             try:
                 review_obj = models.DealerReview(name = review["name"], 
@@ -69,6 +140,16 @@ def get_dealer_reviews_by_id_from_cf(url, dealerId):
 # def analyze_review_sentiments(text):
 # - Call get_request() with specified arguments
 # - Get the returned sentiment label such as Positive or Negative
-
+def analyze_review_sentiments(dealerreview, **kwargs):
+    #API_KEY=""
+    #NLU_URL=''
+    #params = json.dumps({"text": dealerreview, "features": {"sentiment": {}}})
+    #response = requests.post(NLU_URL,data=params,headers={'Content-Type':'application/json'},auth=HTTPBasicAuth("apikey", API_KEY))
+        
+    #try:
+    #    sentiment=response.json()['sentiment']['document']['label']
+    #    return sentiment
+    #except: */
+        return "neutral"
 
 
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -95,7 +95,7 @@ def registration_request(request):
 def get_dealerships(request):
     context = {}
     if request.method == "GET":
-        url = "https://eu-de.functions.appdomain.cloud/api/v1/web/31c7d35e-87ae-4a34-aa3f-52df941d1d68/dealership-package/get-dealership.json"
+        url = "https://eu-de.functions.appdomain.cloud/api/v1/web/31c7d35e-87ae-4a34-aa3f-52df941d1d68/API/dealership.json"
         # Get dealers from the URL
         context = {"dealerships": restapis.get_dealers_from_cf(url)}
         # Concat all dealer's short name
@@ -106,8 +106,9 @@ def get_dealerships(request):
 # Create a `get_dealer_details` view to render the reviews of a dealer
 def get_dealer_details(request, dealer_id):
     context = {}
+    print(" DEALER ID: ", dealer_id)
     if request.method == "GET":
-        url = "https://eu-de.functions.appdomain.cloud/api/v1/web/31c7d35e-87ae-4a34-aa3f-52df941d1d68/dealership-package/get-review"
+        url = "https://eu-de.functions.appdomain.cloud/api/v1/web/31c7d35e-87ae-4a34-aa3f-52df941d1d68/API/review/"+dealer_id
         context = {"reviews":  restapis.get_dealer_reviews_by_id_from_cf(url, dealer_id)}
         return render(request, 'djangoapp/dealer_details.html', context)
 
